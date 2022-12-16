0d29934d69afa11d4d0d14a54053b795669dde7b
matheusgrandi <matheus@matheusgrandi.com.br>
2022-12-16 14:01:02 -0300

    :bug: fix type error

--- src/Post.tsx
+++ src/Post.tsx
@@ -2,7 +2,8 @@ interface IProps {
   author: string;
   content: string
 }
-export function Post(props: Object){
+
+export function Post(props: IProps){
   return (
     <div>
       <h1>{props.author}</h1>
